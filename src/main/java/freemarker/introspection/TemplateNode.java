5dd114d24de080168dd1b0a51b8d298af81b969b
Chris Wong <chwong@epsilon.com>
2014-06-06 09:05:21 -0400

    Javadoc updates

--- src/main/java/freemarker/introspection/TemplateNode.java
+++ src/main/java/freemarker/introspection/TemplateNode.java
@@ -1,5 +1,8 @@
 package freemarker.introspection;
 
+/**
+ * Represents a Freemarker node in a template 
+ */
 public interface TemplateNode {
     /** The start column location of this node in the template */
     int getBeginColumn();
